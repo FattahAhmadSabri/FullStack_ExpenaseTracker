8a18d84ea94436c77bdc5190e585ea7022149884
FattahAhmadSabri <fassabri1@gmail.com>
2026-07-24 12:59:48 +0530

    port added

--- backend/utils/dbConfig.js
+++ backend/utils/dbConfig.js
@@ -1,11 +1,13 @@
 const Sequelize = require("sequelize");
 
-
 const db = process.env.DB_NAME
 const userDb = process.env.DB_USER
 const password = process.env.DB_PASSWORD
+const bdHost = process.env.DB_HOST
+const dbPort =process.env.DB_PORT
 const sequelize = new Sequelize(db, userDb, password, {
-  host: "localhost",
+  host:bdHost,
+  port:dbPort,
   dialect: "mysql",
   logging: false,
 });
